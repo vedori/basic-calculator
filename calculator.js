4463f9071897e28d88f98b0bf2d3259039011d38
null <null@void.box>
2023-12-24 00:14:53 -0500

    added top option bar

--- calculator.js
+++ calculator.js
@@ -1,23 +1,36 @@
-const textField = document.querySelector('.calc-input');
-const buttonInputs = document.querySelectorAll('.number-pad button');
+const textField = document.querySelector(".calc-input");
+const buttonInputs = document.querySelectorAll(".number-pad button");
 const computeButton = document.querySelector(".calc-compute");
 const clearButton = document.querySelector(".calc-clear");
 
-const keys = ['1', '2', '3', ' + ', 
-              '4', '5', '6', ' - ',
-              '7', '8', '9', ' * ',
-              '0', '.', '', ' / ', 
+const keys = [
+  "1",
+  "2",
+  "3",
+  " + ",
+  "4",
+  "5",
+  "6",
+  " - ",
+  "7",
+  "8",
+  "9",
+  " * ",
+  "0",
+  ".",
+  "",
+  " / ",
 ];
 
 // Updates the text when the page is loaded
-let calculation = localStorage.getItem('calculation') || '';
+let calculation = localStorage.getItem("calculation") || "";
 textField.textContent = calculation;
 
 // Updates the calculation when the key is clicked
 // assumes buttons ordered in accordance to the keys array
 for (let index = 0; index < buttonInputs.length; index++) {
   if (keys[index]) {
-    buttonInputs[index].addEventListener("click", function() {
+    buttonInputs[index].addEventListener("click", function () {
       updateCalculation(keys[index]);
     });
   }
@@ -25,26 +38,26 @@ for (let index = 0; index < buttonInputs.length; index++) {
 
 function updateCalculation(value) {
   calculation += value;
-  localStorage.setItem('calculation', calculation);
+  localStorage.setItem("calculation", calculation);
   textField.textContent = calculation;
   console.log(calculation);
 }
 
 function compute() {
   calculation = eval(calculation);
-  localStorage.setItem('calculation', calculation);
+  localStorage.setItem("calculation", calculation);
   textField.textContent = calculation;
   console.log(calculation);
 }
 
 function clearCalculator() {
-  textField.textContent = '';
+  textField.textContent = "";
 
   // resets calculation
-  calculation = '';
-  localStorage.removeItem('calculation');
+  calculation = "";
+  localStorage.removeItem("calculation");
 
-  console.log('clear');
+  console.log("clear");
 }
 
 // Ensures compute and clear run their respective functions when clicked
